70ccf34e768e333d170a69669f9f0c383e127c0e
praabindhp <praabindhp@gmail.com>
2024-06-06 10:55:07 +0530

    HO Functions

--- JavaScript-Practices/Higher_Order/HO-Functions_Arrays.js
+++ JavaScript-Practices/Higher_Order/HO-Functions_Arrays.js
@@ -1,17 +1,17 @@
 const companies = [{ name: "CompanyOne", category: "Finance", start: 1981, end: 2003 },
-    { name: "CompanyTwo", category: "Retail", start: 1992, end: 2008 },
-    { name: "CompanyThree", category: "Auto", start: 1999, end: 2007 },
-    { name: "CompanyFour", category: "Retail", start: 1989, end: 2010 },
-    { name: "CompanyFive", category: "Technology", start: 2009, end: 2014 },
-    { name: "CompanySix", category: "Finance", start: 1987, end: 2010 },
-    { name: "CompanySeven", category: "Auto", start: 1986, end: 1996 },
-    { name: "CompanyEight", category: "Technology", start: 2011, end: 2016 },
-    { name: "CompanyNine", category: "Retail", start: 1981, end: 1989 }
+{ name: "CompanyTwo", category: "Retail", start: 1992, end: 2008 },
+{ name: "CompanyThree", category: "Auto", start: 1999, end: 2007 },
+{ name: "CompanyFour", category: "Retail", start: 1989, end: 2010 },
+{ name: "CompanyFive", category: "Technology", start: 2009, end: 2014 },
+{ name: "CompanySix", category: "Finance", start: 1987, end: 2010 },
+{ name: "CompanySeven", category: "Auto", start: 1986, end: 1996 },
+{ name: "CompanyEight", category: "Technology", start: 2011, end: 2016 },
+{ name: "CompanyNine", category: "Retail", start: 1981, end: 1989 }
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-companies.forEach(function(company) {
+companies.forEach(function (company) {
     console.log(company.name);
 })
 
@@ -26,7 +26,7 @@ const lastedTenYears = companies.filter(company => (company.end - company.start 
 console.log(lastedTenYears);
 
 // Map
-const companyNames = companies.map(function(company) { return company.name; });
+const companyNames = companies.map(function (company) { return company.name; });
 console.log(companyNames);
 
 const testMap = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
